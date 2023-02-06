c1d590b07935a3633a5b8a25c4cfdab190a9e653
Gulnaz <kweenmafaka@gmail.com>
2023-02-07 01:24:19 +0600

    3 задача

--- lab33.py
+++ lab33.py
@@ -1,3 +1,9 @@
 from random import randint
+from random import randrange
 
 print("Сколько лет Адилету?", randint(0, 25))
+
+print("Сколько лет маме Адилета?", randrange(38, 40, 56))
+
+family = ['Adilet', 'Gulnaz', 'Aizhan']
+print(list(enumerate(family, start=1)))
